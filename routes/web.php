a95628ecc67c6bbbad021baee07313a2ba90c326
PlanRift <dzikramufidatovid@gmail.com>
2023-03-15 11:29:34 +0700

    recycle bin done

--- routes/web.php
+++ routes/web.php
@@ -31,6 +31,7 @@ Route::get('/', function () {
 
 Route::get('posts', [PostController::class, 'index']);
 Route::get('posts/create', [PostController::class, 'create']);
+Route::get('posts/trash',[PostController::class, 'trash']);
 Route::get('posts/{id}', [PostController::class, 'show']);
 Route::post('posts', [PostController::class, 'store']);
 
