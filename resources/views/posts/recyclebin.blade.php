a19c539699d9429ea488c5e86c072366b024dc14
PlanRift <dzikramufidatovid@gmail.com>
2023-03-24 08:59:13 +0700

    recycle bin

--- resources/views/posts/recyclebin.blade.php
+++ resources/views/posts/recyclebin.blade.php
@@ -1,50 +1,21 @@
-<!DOCTYPE html>
-<html>
+@extends('layouts.app')
+@section('title', "Beranda")
+@section('content')
+<h1>
+    Blog-Ku <a href="{{ url('posts') }}" class="btn btn-warning">Kembali</a>
+</h1>
 
-<head>
-    <title>Blog</title>
+@foreach($posts as $p)
 
-    <link href="{{ asset('bootstrap/css/bootstrap.min.css')}}" rel="stylesheet"
-        integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
-
-    <style>
-    .body {
-        padding: 6px;
-        border-bottom: 2px solid red;
-    }
-
-    span {
-        color: aqua;
-    }
-    </style>
-</head>
-
-<body>
-
-    <div class="container my-4">
-        <h1>
-            Blog-Ku <a href="{{ url('posts') }}" class="btn btn-warning">Kembali</a>
-        </h1>
-
-        @foreach($posts as $p)
-
-        <div class="card my-4">
-            <div class="card-body">
-                <h5 class="card-title">{{ $p->title }}</h5>
-                <p class="card-text">{{ $p->content }}</p>
-                <p class="card-text"><small class="text-muted">Created At {{ date("d M Y H:i", strtotime($p->created_at)) }}
-                    </small> 
-                </p>
-            </div>
-        </div>
-
-        @endforeach
-
+<div class="card my-4">
+    <div class="card-body">
+        <h5 class="card-title">{{ $p->title }}</h5>
+        <p class="card-text">{{ $p->content }}</p>
+        <p class="card-text"><small class="text-muted">Created At {{ date("d M Y H:i", strtotime($p->created_at)) }}
+            </small>
+        </p>
     </div>
+</div>
 
-    <script src="{{ asset('bootstrap/js/bootstrap.bundle.min.js')}}"
-        integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous">
-    </script>
-</body>
-
-</html>
+@endforeach
+@endsection
