11fda2fa637b1af0d7d6bbad58a6be355e460ba1
PlanRift <dzikramufidatovid@gmail.com>
2023-03-24 08:58:49 +0700

    index footer header

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -1,53 +1,24 @@
-<!DOCTYPE html>
-<html>
+@extends('layouts.app')
+@section('title', "Beranda")
+@section('content')
+<h1>
+  Blog-Ku <a href="{{ url('posts/create') }}" class="btn btn-warning">+ Buat Post</a>
 
-<head>
-    <title>Blog</title>
+  <a href="{{ url('posts/trash') }}" class="btn btn-danger">Riwayat Hapus</a>
+</h1>
 
-    <link href="{{ asset('bootstrap/css/bootstrap.min.css')}}" rel="stylesheet"
-        integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+@foreach($posts as $p)
 
-    <style>
-    .body {
-        padding: 6px;
-        border-bottom: 2px solid red;
-    }
+<div class="card my-4">
+  <div class="card-body">
+    <h5 class="card-title">{{ $p->title }}</h5>
+    <p class="card-text">{{ $p->content }}</p>
+    <p class="card-text"><small class="text-muted">Created At {{ date("d M Y H:i", strtotime($p->created_at)) }}
+      </small> </p>
+    <a href="{{ url("posts/$p->slug") }}" class="btn btn-primary">Selengkapnya</a>
+    <a href="{{ url("posts/$p->slug/edit") }}" class="btn btn-warning">Edit</a>
+  </div>
+</div>
 
-    span {
-        color: aqua;
-    }
-    </style>
-</head>
-
-<body>
-
-    <div class="container my-4">
-        <h1>
-            Blog-Ku <a href="{{ url('posts/create') }}" class="btn btn-warning">+ Buat Post</a>
-
-            <a href="{{ url('posts/trash') }}" class="btn btn-danger">Riwayat Hapus</a>
-        </h1>
-
-        @foreach($posts as $p)
-
-        <div class="card my-4">
-            <div class="card-body">
-                <h5 class="card-title">{{ $p->title }}</h5>
-                <p class="card-text">{{ $p->content }}</p>
-                <p class="card-text"><small class="text-muted">Created At {{ date("d M Y H:i", strtotime($p->created_at)) }}
-                    </small> </p>
-                <a href="{{ url("posts/$p->slug") }}" class="btn btn-primary">Selengkapnya</a>
-                <a href="{{ url("posts/$p->slug/edit") }}" class="btn btn-warning">Edit</a>
-            </div>
-        </div>
-
-        @endforeach
-
-    </div>
-
-    <script src="{{ asset('bootstrap/js/bootstrap.bundle.min.js')}}"
-        integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous">
-    </script>
-</body>
-
-</html>
+@endforeach
+@endsection
